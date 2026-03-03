4ad979f0e678608de34ef97a61465fbe72f97a5e
andrymendoza <andry.mendoza pm23232@cecyte.edu.mex>
2026-03-03 07:36:48 -0600

    feat: estructura inicial del projecto

---
+++ src/main/java/com/mycompany/practicagit_lig/PracticaGit_Lig.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicagit_lig;
+
+/**
+ *
+ * @author Alumno
+ */
+public class PracticaGit_Lig {
+
+    public static void main(String[] args) {
+        System.out.println("---Systema de gestion de alumnos---");
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
